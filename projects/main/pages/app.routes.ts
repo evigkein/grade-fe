d00f11765e8725aa24e45f21ae12045a74882e71
Anton Ilin <a.ilin@pointpay.io>
2025-12-02 14:16:26 -0800

    fast

--- projects/main/pages/app.routes.ts
+++ projects/main/pages/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { LayoutComponent } from '../../domain/features/layout/layout.component';
-import { homeRoute } from './routes/home';
-import { portfolioRoute } from './routes/portfolio';
+import { LayoutComponent } from '../domain/features/layout/layout.component';
+import { homeRoute } from './home/home.route';
+import { portfolioRoute } from './portfolio/portfolio.route';
 
 export const routes: Routes = [
   {
--- projects/main/pages/home/home.route.ts
+++ projects/main/pages/home/home.route.ts
@@ -5,8 +5,6 @@ export const homeRoute: Route = {
   path: ERoute.Main,
   pathMatch: 'full',
   loadComponent: () =>
-    import('../../../pages/home/components/entry/home.component').then(
-      (m) => m.PageHomeComponent
-    ),
-  // canActivate: [authGuard]
+    import('./components/entry/home.component')
+      .then((m) => m.PageHomeComponent),
 };
